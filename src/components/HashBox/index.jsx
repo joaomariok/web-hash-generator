57702bbdf8bfd694eb768ca2f4b36155282e85df
João Magalhães <joaomariomag@gmail.com>
2021-11-25 17:50:33 -0300

    Delete useless files and create skeleton for project

---
+++ src/components/HashBox/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function HashBox() {
+    return (
+        <div className={styles.hashBoxWrapper}>
+            <h1>HashBox</h1>
+        </div>
+    )
+}
